25886252fcc3e232e1cd6b3c491c75b5ac0e945f
Kevin14otieno <92913295+Kevin14otieno@users.noreply.github.com>
2024-03-31 18:29:44 +0300

    Add files via upload

---
+++ cv.php
@@ -0,0 +1,34 @@
+
+
+<div class="container">
+    <h1>Forename SURNAME</h1>
+    <p>E-mail: professional email address</p>
+    <p>Tel: UK landline or mobile</p>
+    <hr>
+
+    <h2>Education and Qualifications</h2>
+    <p>2000-2003 University/Universities - Degree and Subject</p>
+    <p>Location: City, Country (applicable additional info)</p>
+
+    <h2>Work Experience</h2>
+    <h3>Sep-07 – Aug-10 Official Company Name, City, Country</h3>
+    <p>Job title</p>
+    <ul>
+      <li>Please use 3-4 bullets maximum to describe your job function & responsibilities</li>
+      <li>Concentrate on your achievements, and what you have distinctly contributed to in each role, using quantitative examples where possible</li>
+    
+    </ul>
+
+    <h3>Jun-05 – Sep-07 Official Company Name, City, Country</h3>
+    <p>Job title</p>
+    <ul>
+      <li>Make sure your work experience comes to life, consider what someone reading your CV would be most interested in</li>
+      <li>Avoid any negativity or shortcomings on your CV that may raise the wrong questions</li>
+   
+    </ul>
+    <h2>Additional Information</h2>
+    <p>Interests: Concentrate on activities you participate in and are willing to talk about. Highlight achievements in those activities.</p>
+    <p>Achievements: List academic or other achievements here.</p>
+    <p>Nationality: Your nationality, dual nationality, and any additional work authorization if applicable</p>
+    <p>Languages: Languages other than English and ability level (e.g., German - fluent)</p>
+  </div>
